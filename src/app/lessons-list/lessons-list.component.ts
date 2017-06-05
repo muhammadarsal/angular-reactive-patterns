bf4f42378ed67701ee04f06a16139917e1e788e1
Muhammad Arsal Abdullah <arsal@plerosys.com>
2017-06-05 10:20:50 +0500

    --

--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -3,41 +3,34 @@ import {globalEventBus, Observer, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON} from "
 import {Lesson} from "../shared/model/lesson";
 
 @Component({
-    selector: 'lessons-list',
-    templateUrl: './lessons-list.component.html',
-    styleUrls: ['./lessons-list.component.css']
+	selector: 'lessons-list',
+	templateUrl: './lessons-list.component.html',
+	styleUrls: ['./lessons-list.component.css']
 })
 export class LessonsListComponent implements Observer {
+	lessons: Lesson[] = [];
 
-    lessons: Lesson[] =[];
+	constructor() {
+		console.log('lesson list component is registered as observer ..');
+		globalEventBus.registerObserver(LESSONS_LIST_AVAILABLE, this);
 
-    constructor() {
-        console.log('lesson list component is registered as observer ..');
-        globalEventBus.registerObserver(LESSONS_LIST_AVAILABLE, this);
+		globalEventBus.registerObserver(ADD_NEW_LESSON, {
+			notify: lessonText => {
+				this.lessons.push({
+					id: Math.random(),
+					description: lessonText
+				})
+			}
+		} );
+	}
 
-        globalEventBus.registerObserver(ADD_NEW_LESSON, {
-            notify: lessonText => {
-                this.lessons.push({
-                    id: Math.random(),
-                    description: lessonText
-                })
-            }
-        } );
-    }
+	notify(data: Lesson[]) {
+		console.log('Lessons list component received data ..');
+		this.lessons = data;
+	}
 
-    notify(data: Lesson[]) {
-        console.log('Lessons list component received data ..');
-        this.lessons = data;
-    }
-
-    toggleLessonViewed(lesson:Lesson) {
-        console.log('toggling lesson ...');
-        lesson.completed = !lesson.completed;
-    }
-
-
-
+	toggleLessonViewed(lesson:Lesson) {
+		console.log('toggling lesson ...');
+		lesson.completed = !lesson.completed;
+	}
 }
-
-
-
